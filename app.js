7b8adc28544cdf8ce8a914e5ef39fb874c54a4ed
ganesh <ganeshgajula22@gmail.com>
2020-12-28 13:27:00 +0530

    style: adding styling

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function clickHandler() {
   console.log(billAmount);
   console.log(remainingAmount);
   const currencies = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
-  if (remainingAmount > 0) {
+  if (billAmount > 0 && cashGiven > 0 && remainingAmount > 0) {
     for (let i = 0; i < currencies.length; i++) {
       let notes = remainingAmount / currencies[i];
       console.log("No. of " + currencies[i] + " notes is ", Math.floor(notes));
@@ -32,8 +32,12 @@ function clickHandler() {
         break;
       }
     }
-  } else if (remainingAmount === 0) {
+  } else if (billAmount > 0 && cashGiven > 0 && remainingAmount === 0) {
     alert("Thank you! you paid the exact amount");
+  } else if (billAmount === "" && cashGiven === "") {
+    alert("Please enter the billed amount and cash given by the customer");
+  } else if (billAmount !== "" && cashGiven === "") {
+    alert("Please enter the amount of cash given by the customer");
   } else {
     alert("Cash paid is less than the billed amount");
   }
